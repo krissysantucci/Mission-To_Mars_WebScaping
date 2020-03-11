9727cfe27c8f2c4ebc8005c1c8b8fc6ff6f47287
KrissyD <57197581+krissysantucci@users.noreply.github.com>
2020-03-10 22:00:46 -0700

    still working

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+#mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+
+# connect to mongo db and collection
+@app.route("/")
+def index():
+    mars_results = mongo.db.mars.find_one()
+    return render_template("index.html", mars_results = mars_results)
+
+
+@app.route("/scrape")
+def scrape():
+    mars_results = mongo.db.mars_results
+    mars_data = scrape_mars.scrape()
+    
+    mars_results.mongo.db.collection.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+    #return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,108 @@
+import os
+from bs4 import BeautifulSoup as bs
+import requests
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+import pandas as pd
+import datetime as dt
+
+executable_path = {'executable_path': 'chromedriver.exe'}
+browser = Browser('chrome', **executable_path, headless=True)
+
+
+def mars_news(browser):
+    nasa_url = "https://mars.nasa.gov/news/"
+    browser.visit(nasa_url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+    try:
+        news_element = soup.select_one('ul.item_list li.slide')
+        news_element.find('div', class_='content_title')
+
+        news_title = news_element.find('div', class_='content_title').text
+        news_p = news_element.find('div', class_='article_teaser_body').text
+    except AttributeError:
+        return None, None
+    return news_title, news_p
+
+
+def featured_image(browser):
+    feature_image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(feature_image_url)
+    
+    full_image_button = browser.find_by_id("full_image")
+    full_image_button.click()
+    
+    browser.is_element_present_by_text("more info", wait_time=1)
+    more_info_element = browser.links.find_by_partial_text("more info")
+    more_info_element.click()
+
+    html = browser.html
+    image_soup = bs(html, "html.parser")
+
+    images = image_soup.click_link_by_partial_href("/spaceimages/image")
+    try:
+        images_url = images.get("src")
+    except AttributeError:
+        return None
+    images_url = f"https://www.jpl.nasa.gov{images_url}"
+    return images_url
+    
+def mars_facts():
+    facts_url = 'https://space-facts.com/mars/'
+    try:
+        tables = pd.read_html(facts_url)[0]
+    except BaseException:
+        return None
+    tables.columns=["Description", "Value"]
+    tables.set_index("Description", inplace=True)
+    return tables.to_html(classes="table table-striped")
+
+def hemisphere(browser):
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    hemisphere_image_urls = []
+
+    links = browser.find_by_css("a.product-item h3")
+    for item in range(len(links)):
+        hemisphere = {}
+        
+        # Find Element on Each Loop to Avoid a Stale Element Exception
+        browser.find_by_css("a.product-item h3")[item].click()
+        
+        # Find Sample Image Anchor Tag & Extract <href>
+        sample_element = browser.find_link_by_text("Sample").first
+        hemisphere["img_url"] = sample_element["href"]
+        
+        # Get Hemisphere Title
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        
+        # Append Hemisphere Object to List
+        hemisphere_image_urls.append(hemisphere)
+        
+        # Navigate Backwards
+        browser.back()
+    return hemisphere_image_urls
+    
+def scrape():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser("chrome", **executable_path, headless=False)
+    news_title, news_p = mars_news(browser)
+    images_url = featured_image(browser)
+    facts = mars_facts()
+    hemisphere_image_urls = hemisphere(browser)
+    
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_p,
+        "featured_image": images_url,
+        "mars_facts": facts,
+        "hemispheres": hemisphere_image_urls,
+        }
+    browser.quit()
+    return data 
+    
+if __name__ == "__main__":
+    print(scrape())
